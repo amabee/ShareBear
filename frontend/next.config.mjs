e6449862b092320ebc4a3a716640e0fb1c843bfd
Paul Sho <Cyberkiller.CK@gmail.com>
2025-07-15 15:09:21 +0800

    Implement code changes to enhance functionality and improve performance
    modified the custom data for frontend

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  images: {
+    domains: ["api.unsplash.com", "images.unsplash.com"],
+  },
+};
 
 export default nextConfig;
