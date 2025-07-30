1201d81059471631ed6656af5a59a55ccc6f7e7d
Paul Sho <Cyberkiller.CK@gmail.com>
2025-07-30 19:42:47 +0800

    Delete frontend directory
    Removing this because of the persistent bug / error. but Imma fix this real quick

--- frontend/utils/formatCount.js
+++
@@ -1,9 +0,0 @@
-export function formatCount(number) {
-  if (number >= 1_000_000) {
-    return (number / 1_000_000).toFixed(1).replace(/\.0$/, "") + "M";
-  }
-  if (number >= 1_000) {
-    return (number / 1_000).toFixed(1).replace(/\.0$/, "") + "K";
-  }
-  return number.toString();
-}
