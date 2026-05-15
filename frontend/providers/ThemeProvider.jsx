fac74f4eb58c736e0d685243eed33fd801c04356
CK Dev <ckdev.00002@gmail.com>
2026-05-15 19:17:35 +0800

    FIXES

--- frontend/providers/ThemeProvider.jsx
+++ frontend/providers/ThemeProvider.jsx
@@ -1,18 +1,7 @@
 "use client";
-import { useEffect, useState } from "react";
 import { ThemeProvider } from "next-themes";
 
 function ClientThemeProvider({ children, ...props }) {
-  const [mounted, setMounted] = useState(false);
-
-  useEffect(() => {
-    setMounted(true);
-  }, []);
-
-  if (!mounted) {
-    return <div style={{ visibility: "hidden" }}>{children}</div>;
-  }
-
   return <ThemeProvider {...props}>{children}</ThemeProvider>;
 }
 
