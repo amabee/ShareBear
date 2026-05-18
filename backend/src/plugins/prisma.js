a0a8a709fb679a800484d171e3fb6bdbd851fc3e
amabee <cyberkiller.ck@gmail.com>
2026-05-18 10:59:38 +0800

    fix: remove broken prisma-extension-random causing all model delegates to be undefined

--- backend/src/plugins/prisma.js
+++ backend/src/plugins/prisma.js
@@ -1,10 +1,8 @@
 import fp from "fastify-plugin";
 import { PrismaClient } from "@prisma/client";
-import prismaRandom from "prisma-extension-random";
 
 export default fp(async (fastify, opts) => {
-  // const prisma = new PrismaClient();
-  const prisma = new PrismaClient().$extends(prismaRandom());
+  const prisma = new PrismaClient();
   await prisma.$connect();
 
   fastify.decorate("prisma", prisma);
